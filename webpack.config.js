56e9173b2d1dc099247d7e052ea5a8eea90948ad
Mayur Patel <mayur.patel@mastercard.com>
2017-09-03 20:01:16 +0530

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+const OccurrenceOrderPlugin = new webpack.optimize.OccurrenceOrderPlugin();
+const HotModuleReplacementPlugin = new webpack.HotModuleReplacementPlugin();
+const NoEmitOnErrorsPlugin = new webpack.NoEmitOnErrorsPlugin();
+
+module.exports = {
+	devtool: 'inline-source-map',
+	entry: [
+		'./client/index.js'
+	],
+	output: {
+		path: path.resolve('dist'),
+		filename: 'index_bundle.js'
+	},
+	module: {
+		loaders: [
+			{ test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ }
+		]
+	},
+	plugins: [
+		HtmlWebpackPluginConfig,
+		OccurrenceOrderPlugin,
+		HotModuleReplacementPlugin,
+		NoEmitOnErrorsPlugin
+	]
+}
